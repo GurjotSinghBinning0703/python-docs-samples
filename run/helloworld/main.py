4a5d0469879d4a324c83a4c3d24a9f63d6bbfd55
GBINNING <GurjotSingh.Binning@otto.de>
2021-09-23 10:44:56 +0200

    secret check

--- run/helloworld/main.py
+++ run/helloworld/main.py
@@ -17,15 +17,39 @@
 import os
 
 from flask import Flask
-
+from google.cloud import secretmanager
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
+    project_id = "gcp-it-ec-seoanlt-dev-tki"
     string = "teststring"
+    secret_id = "Singh_test_secret"
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+    # Build the parent name from the project.
+    parent = f"projects/{project_id}"
+    # Create the parent secret.
+    secret = client.create_secret(
+        request={
+            "parent": parent,
+            "secret_id": secret_id,
+            "secret": {"replication": {"automatic": {}}},
+        }
+    )
+    # Add the secret version.
+    version = client.add_secret_version(
+        request={"parent": secret.name, "payload": {"data": b"hello world!"}}
+    )
+    # Access the secret version.
+    response = client.access_secret_version(request={"name": version.name})
+    payload = response.payload.data.decode("UTF-8")
     name = os.environ.get("NAME", "World")
-    return "Hello Singh {}!".format(string)
+
+
+
+    return "Hello Singh {}!".format(payload)
 
 
 if __name__ == "__main__":
